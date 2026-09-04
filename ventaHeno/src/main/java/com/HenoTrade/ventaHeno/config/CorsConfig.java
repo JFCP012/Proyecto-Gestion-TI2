c3c6a14f83cc52961cc2295bab53a224b2d50c48
JFCP012 <juanfecelprieto@gmail.com>
2026-09-04 02:14:47 -0500

    5

--- ventaHeno/src/main/java/com/HenoTrade/ventaHeno/config/CorsConfig.java
+++ ventaHeno/src/main/java/com/HenoTrade/ventaHeno/config/CorsConfig.java
@@ -6,13 +6,22 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://agroheno.vercel.app", "http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://agroheno-git-master-jfcp012s-projects.vercel.app",
+                                "http://localhost:4200" // por si pruebas localmente en Angular
+                )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
